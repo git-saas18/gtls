82c11132a07a6eccb11d89c6a40ea0483057e817
git-saas18 <github@saas18.com>
2025-04-16 23:44:54 +0800

    代理支持socks5h

--- gtls.go
+++ gtls.go
@@ -12,9 +12,9 @@ import (
 	"errors"
 	"math/big"
 	"net"
+	"net/http"
 	"net/url"
 	"strconv"
-	"sync"
 	"time"
 
 	"github.com/caddyserver/certmagic"
@@ -22,7 +22,13 @@ import (
 
 var Https = certmagic.HTTPS
 
-func MagicTLS(domainNames []string) (*tls.Config, error) {
+func HTTPS(domainNames []string, mux http.Handler) error {
+	return certmagic.HTTPS(domainNames, mux)
+}
+func Listen(domainNames []string) (net.Listener, error) {
+	return certmagic.Listen(domainNames)
+}
+func TLS(domainNames []string) (*tls.Config, error) {
 	return certmagic.TLS(domainNames)
 }
 
@@ -62,8 +68,7 @@ func VerifyProxy(proxyUrl string) (*url.URL, error) {
 		return nil, err
 	}
 	switch proxy.Scheme {
-	case "http", "socks5", "https":
-
+	case "http", "https", "socks5", "socks5h":
 		return proxy, nil
 	default:
 		return nil, errors.New("unsupported proxy scheme: " + proxy.Scheme)
@@ -77,119 +82,150 @@ func GetServerName(addr string) string {
 	return host
 }
 
-func generateSerialNumber() (*big.Int, error) {
-	return rand.Int(rand.Reader, new(big.Int).Lsh(big.NewInt(1), 128))
-}
-func CreateRootCert() (*x509.Certificate, *ecdsa.PrivateKey, error) {
-	serialNumber, err := generateSerialNumber()
+func CreateRootCert(key *ecdsa.PrivateKey) (*x509.Certificate, error) {
+	beforDate, err := time.ParseInLocation(time.DateOnly, "2023-03-20", time.Local)
 	if err != nil {
-		return nil, nil, err
+		return nil, err
 	}
-	// 创建 CA 证书模板
-	caTemplate := &x509.Certificate{
-		SerialNumber: serialNumber,
+	afterDate, err := time.ParseInLocation(time.DateOnly, "3023-03-20", time.Local)
+	if err != nil {
+		return nil, err
+	}
+	rootCsr := &x509.Certificate{
+		Version:      3,
+		SerialNumber: big.NewInt(time.Now().Unix()),
 		Subject: pkix.Name{
 			Country:            []string{"CN"},
-			Organization:       []string{"MITM Proxy Co"},
-			OrganizationalUnit: []string{"MITM"},
-			CommonName:         "MITM Proxy CA",
+			Province:           []string{"Shanghai"},
+			Locality:           []string{"Shanghai"},
+			Organization:       []string{"GoSpider"},
+			OrganizationalUnit: []string{"GoSpiderProxy"},
+			CommonName:         "Gospider Root CA",
 		},
-		NotBefore:             time.Now(),
-		NotAfter:              time.Now().AddDate(100, 0, 0), // 100年有效期
-		KeyUsage:              x509.KeyUsageCertSign | x509.KeyUsageCRLSign,
-		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth, x509.ExtKeyUsageClientAuth},
+		NotBefore:             beforDate,
+		NotAfter:              afterDate,
+		BasicConstraintsValid: true,
 		IsCA:                  true,
-		BasicConstraintsValid: true,
+		MaxPathLen:            1,
+		MaxPathLenZero:        false,
+		KeyUsage:              x509.KeyUsageCertSign | x509.KeyUsageCRLSign,
 	}
-	// 生成 CA 私钥
-	caPrivKey, err := ecdsa.GenerateKey(elliptic.P384(), rand.Reader)
+	rootDer, err := x509.CreateCertificate(rand.Reader, rootCsr, rootCsr, key.Public(), key)
 	if err != nil {
-		return nil, nil, err
+		return nil, err
 	}
-	// 生成自签名 CA 证书
-	caCertBytes, err := x509.CreateCertificate(rand.Reader, caTemplate, caTemplate, &caPrivKey.PublicKey, caPrivKey)
-	if err != nil {
-		return nil, nil, err
-	}
-	caCert, _ := x509.ParseCertificate(caCertBytes)
-	return caCert, caPrivKey, nil
+	return x509.ParseCertificate(rootDer)
 }
-func CreateCertWithName(serverName string) (tls.Certificate, error) {
-	caCert, err := LoadCert(CrtFile)
-	if err != nil {
-		return tls.Certificate{}, err
-	}
-	caPrivKey, err := LoadCertKey(KeyFile)
-	if err != nil {
-		return tls.Certificate{}, err
-	}
-	serialNumber, err := generateSerialNumber()
-	if err != nil {
-		return tls.Certificate{}, err
-	}
-	// 服务器证书模板
-	serverTemplate := &x509.Certificate{
-		SerialNumber: serialNumber,
-		Subject: pkix.Name{
-			Country:            []string{"CN"},
-			Organization:       []string{"MITM Proxy Co"},
-			OrganizationalUnit: []string{"MITM"},
-		},
+
+func CreateCertKey() (*ecdsa.PrivateKey, error) {
+	return ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+}
+func CreateCertWithCN(rootCert *x509.Certificate, key *ecdsa.PrivateKey, commonName string) (*x509.Certificate, error) {
+	csr := &x509.Certificate{
+		Version:               3,
+		SerialNumber:          big.NewInt(time.Now().Unix()),
+		Subject:               rootCert.Subject,
+		IPAddresses:           []net.IP{net.IPv4(127, 0, 0, 1)},
 		NotBefore:             time.Now(),
-		NotAfter:              time.Now().AddDate(100, 0, 0), // 1年有效期
-		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
-		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+		NotAfter:              time.Now().AddDate(1000, 0, 0),
 		BasicConstraintsValid: true,
+		IsCA:                  false,
+		KeyUsage:              x509.KeyUsageDigitalSignature | x509.KeyUsageKeyEncipherment,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth, x509.ExtKeyUsageClientAuth},
 	}
-	if serverName == "" {
-		serverTemplate.IPAddresses = []net.IP{net.IPv4(127, 0, 0, 1), net.IPv6loopback}
-	} else {
-		serverTemplate.DNSNames = []string{serverName}
+	csr.IPAddresses = []net.IP{}
+	if commonName != "" {
+		if ip, ipType := ParseHost(commonName); ipType == 0 {
+			csr.Subject.CommonName = commonName
+			csr.DNSNames = []string{commonName}
+		} else {
+			csr.IPAddresses = append(csr.IPAddresses, ip)
+		}
 	}
+	der, err := x509.CreateCertificate(rand.Reader, csr, rootCert, key.Public(), key)
+	if err != nil {
+		return nil, err
+	}
+	return x509.ParseCertificate(der)
+}
 
-	// 生成服务器私钥（ECDSA 更快，RSA 兼容性更好）
-	serverPrivKey, err := ecdsa.GenerateKey(elliptic.P384(), rand.Reader)
+func CreateCertWithCert(rootCert *x509.Certificate, key *ecdsa.PrivateKey, preCert *x509.Certificate) (*x509.Certificate, error) {
+	if preCert.DNSNames == nil && preCert.Subject.CommonName != "" {
+		preCert.DNSNames = []string{preCert.Subject.CommonName}
+	}
+	rootCert.Subject.CommonName = preCert.Subject.CommonName
+	csr := &x509.Certificate{
+		Version:               3,
+		SerialNumber:          big.NewInt(time.Now().Unix()),
+		Subject:               rootCert.Subject,
+		DNSNames:              preCert.DNSNames,
+		IPAddresses:           preCert.IPAddresses,
+		NotBefore:             time.Now(),
+		NotAfter:              time.Now().AddDate(1000, 0, 0),
+		BasicConstraintsValid: true,
+		IsCA:                  false,
+		KeyUsage:              x509.KeyUsageDigitalSignature | x509.KeyUsageKeyEncipherment,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth, x509.ExtKeyUsageClientAuth},
+	}
+	if len(preCert.DNSNames) > 0 {
+		csr.Subject.CommonName = preCert.DNSNames[0]
+	}
+	der, err := x509.CreateCertificate(rand.Reader, csr, rootCert, key.Public(), key)
+	if err != nil {
+		return nil, err
+	}
+	return x509.ParseCertificate(der)
+}
+func CreateProxyCertWithName(serverName string) (tlsCert tls.Certificate, err error) {
+	crt, err := LoadCert(CrtFile)
+	if err != nil {
+		return tlsCert, err
+	}
+	key, err := LoadCertKey(KeyFile)
+	if err != nil {
+		return tlsCert, err
+	}
+	cert, err := CreateCertWithCN(crt, key, serverName)
+	if err != nil {
+		return tlsCert, err
+	}
+	return CreateTlsCert(cert, key)
+}
+func CreateProxyCertWithCert(crt *x509.Certificate, key *ecdsa.PrivateKey, preCert *x509.Certificate) (tlsCert tls.Certificate, err error) {
+	if crt == nil {
+		crt, err = LoadCert(CrtFile)
+		if err != nil {
+			return tlsCert, err
+		}
+	}
+	if key == nil {
+		key, err = LoadCertKey(KeyFile)
+		if err != nil {
+			return tlsCert, err
+		}
+	}
+	cert, err := CreateCertWithCert(crt, key, preCert)
+	if err != nil {
+		return tlsCert, err
+	}
+	return CreateTlsCert(cert, key)
+}
+func CreateTlsCert(cert *x509.Certificate, key *ecdsa.PrivateKey) (tls.Certificate, error) {
+	keyFile, err := GetCertKeyData(key)
 	if err != nil {
 		return tls.Certificate{}, err
 	}
-	// 用 CA 签发服务器证书
-	serverCertBytes, err := x509.CreateCertificate(rand.Reader, serverTemplate, caCert, &serverPrivKey.PublicKey, caPrivKey)
-	if err != nil {
-		return tls.Certificate{}, err
-	}
-	serverCert, _ := x509.ParseCertificate(serverCertBytes)
-	return tls.X509KeyPair(GetCertData(serverCert), GetCertKeyData(serverPrivKey))
+	return tls.X509KeyPair(GetCertData(cert), keyFile)
 }
-
-var cacheCert sync.Map
-
-func GetCertConfigForClient(config *tls.Config) *tls.Config {
-	return &tls.Config{
-		GetConfigForClient: func(chi *tls.ClientHelloInfo) (*tls.Config, error) {
-			if certData, ok := cacheCert.Load(chi.ServerName); ok {
-				return certData.(*tls.Config), nil
-			}
-			cert, err := CreateCertWithName(chi.ServerName)
-			if err != nil {
-				return nil, err
-			}
-			cf := config.Clone()
-			cf.Certificates = []tls.Certificate{cert}
-			cacheCert.Store(chi.ServerName, cf)
-			return cf, nil
-		},
-	}
-}
-func MergeCert(cert *x509.Certificate, key *ecdsa.PrivateKey) (tls.Certificate, error) {
-	return tls.X509KeyPair(GetCertData(cert), GetCertKeyData(key))
-}
-
 func GetCertData(cert *x509.Certificate) []byte {
 	return pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: cert.Raw})
 }
-func GetCertKeyData(key *ecdsa.PrivateKey) []byte {
-	keyDer, _ := x509.MarshalECPrivateKey(key)
-	return pem.EncodeToMemory(&pem.Block{Type: "EC PRIVATE KEY", Bytes: keyDer})
+func GetCertKeyData(key *ecdsa.PrivateKey) ([]byte, error) {
+	keyDer, err := x509.MarshalECPrivateKey(key)
+	if err != nil {
+		return nil, err
+	}
+	return pem.EncodeToMemory(&pem.Block{Type: "EC PRIVATE KEY", Bytes: keyDer}), nil
 }
 func LoadCertKey(data []byte) (*ecdsa.PrivateKey, error) {
 	block, _ := pem.Decode(data)
@@ -202,37 +238,21 @@ func LoadCert(data []byte) (*x509.Certificate, error) {
 
 type AddrType int
 
-func (at AddrType) String() string {
-	switch at {
-	case AutoIp:
-		return "AutoIp"
-	case Ipv4:
-		return "Ipv4"
-	case Ipv6:
-		return "Ipv6"
-	case UnknownIp:
-		return "UnknownIp"
-	default:
-		return "Undefined"
-	}
-}
-
 const (
-	AutoIp AddrType = iota
+	Auto AddrType = iota
 	Ipv4
 	Ipv6
-	UnknownIp
 )
 
 func ParseIp(ip net.IP) AddrType {
 	if ip != nil {
 		if ip4 := ip.To4(); ip4 != nil {
-			return Ipv4
+			return 4
 		} else if ip6 := ip.To16(); ip6 != nil {
-			return Ipv6
+			return 6
 		}
 	}
-	return UnknownIp
+	return 0
 }
 func GetHost(addrTypes ...AddrType) net.IP {
 	hosts := GetHosts(addrTypes...)
